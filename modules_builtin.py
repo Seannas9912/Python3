f49a628f0e48d6e70e522b308dcb6bf6693c3cc3
Seannas9912 <seanrussell9099@gmail.com>
2025-07-01 09:20:20 +0200

    This is Lesson 3 ,the previous one is Lesson 2.

---
+++ modules_builtin.py
@@ -0,0 +1,19 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+print(math.pi)
+print("\n")
+
+print(random.randint(0, 100))
+print("\n")
+
+print(datetime.datetime.now())
+print("\n")
+
+print(os.getcwd())
+print("\n")
+
+print(sys.version)
