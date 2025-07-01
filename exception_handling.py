9e03c518f7af2c9dc9a60447e48671934478477b
Seannas9912 <seanrussell9099@gmail.com>
2025-07-01 10:09:17 +0200

    Lesson 6

--- exception_handling.py
+++ exception_handling.py
@@ -8,7 +8,12 @@
     
 try:
     print(x)
-except:
-    print("Something went wrong")
-finally:
-    print("The 'try except' is finished")
+except NameError:
+    print("Variable x is not defined")
+else:
+    print("Everything went wrong")
+    
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
